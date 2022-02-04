2bd8bb9d3b4f763540ec8bcf894a5b84098f26cf
Yağmur Doğan <yagmurdogan00@gmail.com>
2022-02-04 16:31:00 +0300

    added scoreScreen showing best score

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Phaser from "phaser";
 import PlayScene from './scenes/PlayScene';
 import MenuScene from './scenes/MenuScene';
 import PreloadScene from "./scenes/PreloadScene";
+import ScoreScene from "./scenes/ScoreScene";
 
 const WIDTH = 800;
 const HEIGHT = 600;
@@ -14,7 +15,7 @@ const SHARED_CONFIG = {
   startPosition: WITCH_POSITION
 }
 
-const Scenes = [PreloadScene, MenuScene, PlayScene];
+const Scenes = [PreloadScene, MenuScene, ScoreScene, PlayScene];
 const createScene = Scene => new Scene(SHARED_CONFIG)
 const initScenes = () => Scenes.map(createScene)
 
